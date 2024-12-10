74dff9a531339e246e5c8af0371fd7d5b070c214
dodi1772 <zsigmond.dorian01@gmail.com>
2024-12-10 09:12:20 +0100

    Done

--- hogomb/Program.cs
+++ hogomb/Program.cs
@@ -9,56 +9,92 @@
 			bool fut = true;
             while (fut)
             {
-                Console.WriteLine("\t1. Elemek hozzáadása\n\t2. Tartalom megtekintése\n\t 3. Elem eltávolítása\n\t4. Terminado");
-                Console.Write("Add meg a választásod: ");
-				int valasz=Convert.ToInt32(Console.ReadLine());
-				switch (valasz)
+				try
 				{
-					case 1:
-						bool futas=true;
-                        while (futas)
-                        {
+					Console.WriteLine("\t1. Elemek hozzáadása\n\t2. Tartalom megtekintése\n\t 3. Elem eltávolítása\n\t4. Terminado");
+					Console.Write("Add meg a választásod: ");
+					int valasz = Convert.ToInt32(Console.ReadLine());
+					switch (valasz)
+					{
+						case 1:
+							bool futas = true;
+							while (futas)
+							{
+								try
+								{
+									Console.Write("Mi az elem neve: ");
+									string beker = Console.ReadLine();
+									if (beker == "")
+									{
+										throw new Exception("A bemenet nem lehet üres.");
+									}
+									Console.Write("Hány darab: ");
+									int menny = Convert.ToInt32(Console.ReadLine);
+									Console.Write("Van még elem(i/n): ");
+									char vane = Convert.ToChar(Console.ReadLine);
+									if (vane == 'n')
+									{
+										futas = false;
+									}
+									elemek.Add(beker);
+									mennyisegek.Add(menny);
+								}
+								catch (OverflowException)
+								{
+									Console.WriteLine("Túllépted a határt.");
+								}
+								catch (FormatException)
+								{
+									Console.WriteLine("Hiba: Rossz bemeneti formátum.");
+								}
+								catch (Exception ex)
+								{
+									Console.WriteLine($"Hiba: {ex.Message}");
+								}
+							}
+							break;
+						case 2:
+							Console.WriteLine("Lista tartalma:");
+							Console.WriteLine(string.Format("Elemek a hógömbben: ({0}).", string.Join(", ", elemek)));
+							Console.WriteLine(string.Format("Elemek mennyisége: ({0}).", string.Join(", ", mennyisegek)));
+							break;
+						case 3:
 							try
 							{
-								Console.Write("Mi az elem neve: ");
-								string beker = Console.ReadLine();
-								if (beker == "")
-								{
-									throw new Exception("A bemenet nem lehet üres.");
-								}
-								Console.Write("Hány darab: ");
-								int menny = Convert.ToInt32(Console.ReadLine);
-								Console.Write("Van még elem(i/n): ");
-								char vane = Convert.ToChar(Console.ReadLine);
-                                if (vane=='n')
+								Console.Write("Melyik elem eltávolítása: ");
+								string bekerElem = Console.ReadLine();
+                                if (bekerElem=="")
                                 {
-									futas = false;
+									throw new Exception("A bemenet nem lehet üres");
                                 }
-								elemek.Add(beker);
-								mennyisegek.Add(menny);
-                            }
-							catch (OverflowException)
-							{
-								Console.WriteLine("Túllépted a határt.");
-							}
-							catch (FormatException)
-							{
-								Console.WriteLine("Hiba: Rossz bemeneti formátum.");
+                                if (elemek.Contains(bekerElem))
+								{
+                                    int elemIndex=elemek.IndexOf(bekerElem);
+									elemek.Remove(bekerElem);
+									mennyisegek.RemoveAt(elemIndex);
+                                }
+								else
+								{
+									throw new Exception("A megadott elem nincs a listában.");
+								}
 							}
 							catch (Exception ex)
 							{
-								Console.WriteLine($"Hiba: {ex.Message}");
-							}
-						}
-						break;
-					case 2:
-                        Console.WriteLine("Lista tartalma:");
-						Console.WriteLine(string.Format("Elemek a hógömbben: ({0}).", string.Join(", ", elemek)));
-						Console.WriteLine(string.Format("Elemek mennyisége: ({0}).", string.Join(", ", mennyisegek)));
-						break;
-					case 3:
-
+                                Console.WriteLine($"Hiba: {ex.Message}");
+                            }
+                            break;
+						case 4:
+							fut = false;
+							break;
+						default:
+                            Console.WriteLine("Adj meg érvényes bemenetet.");
+							break;
+                    }
 				}
+				catch(FormatException)
+				{
+                    Console.WriteLine("Hibás bemeneti formátum.");
+                }
 
 			}
         }
